0bb0c9914871e9d3e7e6b81b54769fcb79848799
Aerish369 <aryalaerish369@gmail.com>
2023-08-12 14:08:49 +0545

    Added multithreading

--- multithreading.py
+++ multithreading.py
@@ -1,23 +1,31 @@
 import time
 import threading
+import concurrent.futures
 
 start = time.perf_counter()
 
 def doingWork(seconds):
     print(f"Sleeping for {seconds} secs")
     time.sleep(seconds)
-    print("Done Sleeping...")
+    return f"Done Sleeping...{seconds}"
+
+with concurrent.futures.ThreadPoolExecutor() as executor:
+    secs = [ 5, 4, 3, 4, 6]
+    results = [executor.submit(doingWork, sec) for sec in secs]
+    
+    for f in concurrent.futures.as_completed(results):
+        print(f.result())
 
 
-threads = []
+#* threads = []
 
-for i in range(10):
-    t = threading.Thread(target=doingWork, args = [1.5])
-    t.start()
-    threads.append(t)
+#* for i in range(10):
+#*     t = threading.Thread(target=doingWork, args = [1.5])
+#*     t.start()
+#*     threads.append(t)
 
-for thread in threads:
-    thread.join()
+#* for thread in threads:
+#*     thread.join()
 
 
 finish = time.perf_counter()
